dbed7a6024c505e6f174e1011abff9cab5ff8955
joeshakely <joeshakely@gmail.com>
2024-11-25 15:48:06 -0800

    Update automate-secrets.sh to create EAS secret with improved clarity

--- automate-secrets.sh
+++ automate-secrets.sh
@@ -24,8 +24,8 @@ if [ ! -f eas.json ]; then
   exit 1
 fi
 
-# Step 5: Create the environment variable
-eas env:create --environment production --name GOOGLE_SERVICE_INFO_PLIST --type file --value ./GoogleServiceInfo.b64 --scope project --visibility sensitive --non-interactive
+# Step 5: Create the EAS secret
+eas env:create --environment production --name GOOGLE_SERVICE_INFO_PLIST --type file --value ./GoogleServiceInfo.b64 --scope project  --visibility sensitive --non-interactive
 
 # Step 6: Verify the variable
 eas env:list production
