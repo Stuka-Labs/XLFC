4b0ad822067a21d32adb971b8fd7331fd9edaf69
joeshakely <joeshakely@gmail.com>
2024-11-25 18:16:24 -0800

    Add Android package configuration to app.config.js

--- app.config.js
+++ app.config.js
@@ -9,6 +9,9 @@ export default {
             googleServicesFile: "./assets/GoogleService-Info.plist",
             bundleIdentifier: "com.stuka.xlfc",
         },
+        "android": {
+            "package": "com.stuka.xlfc"
+        },
         extra: {
             eas: {
                 projectId: "5a5b1fa7-78ad-49d8-9758-76ba0b79c7a2",
