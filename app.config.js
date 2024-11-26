a45519398a7f8422d16c5a80c165f3132b5632be
joeshakely <joeshakely@gmail.com>
2024-11-25 20:04:02 -0800

    Update app.config.js for Firebase integration and Android configuration

--- app.config.js
+++ app.config.js
@@ -1,22 +1,24 @@
 import 'dotenv/config';
 
 export default {
-    expo: {
-        name: "XLFC",
-        slug: "XLFC",
-        scheme: "xlfc",
-        ios: {
-            "supportsTablet": true,
-            googleServicesFile: "./assets/GoogleService-Info.plist",
-            bundleIdentifier: "com.stuka.xlfc",
-        },
-        "android": {
-            "package": "com.stuka.xlfc"
-        },
-        extra: {
-            eas: {
-                projectId: "5a5b1fa7-78ad-49d8-9758-76ba0b79c7a2",
-            },
-        },
+  expo: {
+    name: "XLFC",
+    slug: "XLFC",
+    scheme: "xlfc",
+    ios: {
+      useFrameworks: "static", // Required for Firebase in iOS builds
+      supportsTablet: true,
+      googleServicesFile: "./assets/GoogleService-Info.plist", // Firebase iOS configuration
+      bundleIdentifier: "com.stuka.xlfc", // Unique iOS app ID
     },
+    android: {
+      package: "com.stuka.xlfc", // Unique Android app ID
+      googleServicesFile: "./assets/google-services.json", // Firebase Android configuration
+    },
+    extra: {
+      eas: {
+        projectId: "5a5b1fa7-78ad-49d8-9758-76ba0b79c7a2", // EAS project ID
+      },
+    },
+  },
 };
