a4ef0b9cc0fd948cc80895c8b5a4724a23b49a82
joeshakely <joeshakely@gmail.com>
2024-12-04 14:10:15 -0800

    Refactor app.config.js to remove unused environment variables

--- app.config.js
+++ app.config.js
@@ -35,14 +35,7 @@ export default {
     extra: {
       eas: {
         projectId: "ccb80d1b-1ec1-45d3-8c60-a6e093bfb193",
-      },
-      apiUrl: process.env.API_URL,
-      DEV_API_DOMAIN: process.env.DEV_API_DOMAIN,
-      PROD_API_DOMAIN: process.env.PROD_API_DOMAIN,
-      DEV_APP_NAME: process.env.DEV_APP_NAME,
-      PROD_APP_NAME: process.env.PROD_APP_NAME,
-      DEV_USER_EMAIL: process.env.DEV_USER_EMAIL,
-      DEV_USER_PASSWORD: process.env.DEV_USER_PASSWORD,
+      }
     },
   },
 };
