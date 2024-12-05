f4643846be23892c3a9326f20a8aecf48cd2e547
joeshakely <joeshakely@gmail.com>
2024-12-04 17:56:34 -0800

    Update Google Services file path to use environment variable in app.config.js

--- app.config.js
+++ app.config.js
@@ -37,7 +37,7 @@ export default ({ config }) => {
         useFrameworks: "static", // Required for Firebase integration
         supportsTablet: true,
         bundleIdentifier: "com.stuka.xlfc",
-        googleServicesFile: "./GoogleService-Info.plist",
+        googleServicesFile: process.env.GOOGLE_SERVICES_PLIST
       },
       android: {
         package: "com.stuka.xlfc", // Unique Android identifier
